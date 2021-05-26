f2ed62f9c425b0c192809d1b17df7d5c4098f23f
Victor Farrell <jvictorfarrell@gmail.com>
2021-05-26 17:22:44 -0400

    Updates

---
+++ precache-manifest.d60de6d22c7d52e9b4ba5b222d76156b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe426c970580c0dfeec0a4ae0d80b7a8",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "26cf95cedd335e56d5bb",
+    "url": "/developerFolio/static/css/3.e5ea9aec.chunk.css"
+  },
+  {
+    "revision": "d73e4b2042761bef70a9",
+    "url": "/developerFolio/static/css/4.96ccde37.chunk.css"
+  },
+  {
+    "revision": "a32c86b0b471784c1ad6",
+    "url": "/developerFolio/static/css/main.c2e89c24.chunk.css"
+  },
+  {
+    "revision": "d23983750770ca3d8cee",
+    "url": "/developerFolio/static/js/2.91bd183b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.91bd183b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26cf95cedd335e56d5bb",
+    "url": "/developerFolio/static/js/3.00f08d61.chunk.js"
+  },
+  {
+    "revision": "d73e4b2042761bef70a9",
+    "url": "/developerFolio/static/js/4.f8c2dfab.chunk.js"
+  },
+  {
+    "revision": "a32c86b0b471784c1ad6",
+    "url": "/developerFolio/static/js/main.c11c0e40.chunk.js"
+  },
+  {
+    "revision": "816e0c336dfb11bfbf7b",
+    "url": "/developerFolio/static/js/runtime-main.c6e78778.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/developerFolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/developerFolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "c3598733fd352f2ab9bbe0fb193b17a9",
+    "url": "/developerFolio/static/media/cmsAward1.c3598733.png"
+  },
+  {
+    "revision": "a1ef8507d67303f55fca5f76b774ba0b",
+    "url": "/developerFolio/static/media/cmsfooter.a1ef8507.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "614161f68a910a987724cf355c20614c",
+    "url": "/developerFolio/static/media/genworth.614161f6.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b248d3716ef2ed329faf00dd1a55e143",
+    "url": "/developerFolio/static/media/qmars-award.b248d371.png"
+  },
+  {
+    "revision": "757c4b14d1ddb472b32368d311ff2f78",
+    "url": "/developerFolio/static/media/safe4badge.757c4b14.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "7e2b818ec2e5af15c38fc08469c96695",
+    "url": "/developerFolio/static/media/vfLogo.7e2b818e.png"
+  },
+  {
+    "revision": "9734c417ba3979855efad4f240e314a5",
+    "url": "/developerFolio/static/media/wmLogo.9734c417.png"
+  }
+]);
